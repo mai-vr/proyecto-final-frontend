b989bf7014dec12398539e4541eef2f52f62cd39
maitereynoso <maitereynosov@gmail.com>
2026-02-11 10:57:06 -0300

    change: details on aside styles and add functions on main

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './styles/main.css'
 import { Aside } from './components/Aside'
 import { Chats } from './components/Chats'
+import { useState } from 'react'
 
+const SelectedContact = () => {
+  const [activeContact, setActiveContact] = useState("")
+  return (
+    <>
+      <Aside setActiveContact={setActiveContact}/>
+      <Chats contact={activeContact}/>
+    </>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <main className='app'>
-      <Aside />
-      <Chats />
+      <SelectedContact />
     </main>
   </StrictMode>,
 )
