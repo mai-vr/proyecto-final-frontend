8a7130fe0dc867a63b834db2bf3f339e0711fba3
maitereynoso <maitereynosov@gmail.com>
2026-02-10 21:16:49 -0300

    add: basic structure of chat section

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react"
+
 const Chats = () =>{
+
+    const [messages, setMessages] = useState("")
+
     return (
-        <h1>Hola chat!</h1>
+        <section className="chat">
+            <header>
+                <h2>
+                    Nombre Apellido
+                </h2>
+            </header>
+            <p>
+                Última conexión: 1 minuto
+            </p>
+            <div className="chat-body">
+                {
+                    messages.map((message) => (
+                        <div>
+                            {message.author} : {message.text}
+                            <p>
+                                {message.time}
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder="Escribir mensaje..." />
+                <button>
+                    Enviar
+                </button>
+            </div>
+        </section>
     )
 }
 
