84ce6e6477adf97fe100be96c552f064a0c2a284
maitereynoso <maitereynosov@gmail.com>
2026-02-13 16:21:47 -0300

    refact: the use of context on each component and add styles

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,83 +1,95 @@
-import { useState } from "react"
-import { messages as mockMessages } from '../services/mockApi'
+import { useState, useRef, useEffect, useContext } from "react"
+import { ChatContext } from "../context/ChatContext"
 
-const Chats = ({contact}) =>{
-
+const Chat = () => {
+    
     const [text, setText] = useState("")
-    const [messages, setMessages] = useState(mockMessages)
+    // const [messages, setMessages] = useState(mockUsers.messages)
 
+    const chatBodyRef = useRef(null) // Referencia para mostrar siempre el último mensaje.
+
+    const {selectedUser} = useContext(ChatContext)
+
+    // Manipular el input.
     const handleChangeText = (event) => {
         setText(event.target.value) // En 'text' voy a tener el último valor del input.
     }
 
-    const sendMessage = () => {
-        const currentTime = new Date()
-
-        if (text.length === 0) {
-            return
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            sendMessage()
         }
-
-        const newMesssage = {
-            id: messages.length + 1,
-            author: 'me',
-            time: currentTime.getHours() + ':' + currentTime.getMinutes(), 
-            text: text,
-        }
-
-        setMessages([...messages, newMesssage]) // Mantener los viejos mensajes y agregar los nuevos.
-        
-        localStorage.setItem('messages', JSON.stringify(messages))
-
-        setText("") // Limpiar el input
     }
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter'){
-            sendMessage()
-        } else {
-            return
+    // Funcion para enviar el mensaje (crear el objeto y agregar el objeto a mi lista de mensajes).
+    const sendMessage = () => {
+
+        if (text.length === 0){
+            return // Si no hay mensaje no ejecuto la función.
         }
+        
+        const currentTime = new Date()
+
+        const newMessage = {
+            id: messages.length + 1,  
+            author: "me",
+            time: currentTime.getHours() + ":" + currentTime.getMinutes(),
+            text: text 
+        }
+
+        setText("") // Limpiar el texto.
+    }
+
+    useEffect(() => {
+        if (chatBodyRef.current) { // Cuando enviamos un mensaje actual tiene un scroll top, queremos que el scroll top baje.
+            chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight    
+        }
+    }, []) 
+    // Cuando se modifican los mensajes, se ejecuta lo del primer parámetro.
+
+    if (!selectedUser) {
+        return (
+            <section className="empty-chat-cont">
+                <p className="empty-chat">
+                    Seleccione un contacto para comenzar a conversar
+                </p>
+            </section>
+        )
     }
 
     return (
+        <>
         <section className="chat">
             <header>
                 <h2>
-                    {contact.firstName} {contact.lastName}
+                    {selectedUser.firstName} {selectedUser.lastName}
                 </h2>
-                <p>
-                    Última conexión: 1 minuto
+                <p className={`${selectedUser.status === 'online' ? 'online-chat' : 'offline-chat'}`}>
+                    {selectedUser.status}
                 </p>
             </header>
-
-            <div className="chat-body">
+            <div className="chat-body" ref={chatBodyRef}>
                 {
-                    messages.map((message) => (
-                        <div key={message.id} className={`message ${message.author === 'me' ? 'me' : 'received'}`}>
-                            <p>
-                                {
-                                    message.author === 'me' ? 'Me' : contact.firstName
-                                }: {message.text}
-                            </p>
-                            <p className="timestamp">
-                                {message.time}
-                            </p>
-                        </div>
-                    ))
+                    selectedUser.messages.map((message) => 
+                    <div key={message.id} className={`message ${message.author === "me" ? "me" : "received"}`}>
+                        <b>{message.author}</b> : {message.text}
+                        <p className="timestamp">{message.time}</p>
+                    </div>
+                    )
                 }
             </div>
             <div className="chat-input">
-                <input type="text" placeholder="Type your message..." onChange={handleChangeText} onKeyDown={handleKeyDown} value={text}/>
+                <input type="text" placeholder="Escribe un mensaje..."  onChange={handleChangeText} onKeyDown={handleKeyDown} value={text}/> 
                 <button onClick={sendMessage}>
                     {
                         text === "" ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M9 5a3 3 0 0 1 3-3h0a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3h0a3 3 0 0 1-3-3z"/><path d="M5 10a7 7 0 0 0 14 0M8 21h8m-4-4v4"/></g></svg> :
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14L21 3m0 0l-6.5 18a.55.55 0 0 1-1 0L10 14l-7-3.5a.55.55 0 0 1 0-1z"/></svg>
                     }
-                    
                 </button>
             </div>
         </section>
+        </>
     )
 }
 
-export { Chats }
+export { Chat }
